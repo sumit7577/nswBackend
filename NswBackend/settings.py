478ee48297c5565be64e13d8c0458ba98c71b7ad
sumit7577 <asmitkumarr082@gmail.com>
2024-07-23 00:10:49 -0700

    allowed host

--- NswBackend/settings.py
+++ NswBackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z1b^c4)ibtp55c%e=@a08la@ljw160c_-zr2w8=vese_89y7&c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
